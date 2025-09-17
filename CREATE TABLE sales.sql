48f61e1b2dd8afc11decfbe03dfa01c4605c5a3a
karlellis2000-cell <karlellis2000@gmail.com>
2025-09-17 16:49:01 +0200

    Update CREATE TABLE sales.sql

--- CREATE TABLE sales.sql
+++ CREATE TABLE sales.sql
@@ -4,7 +4,8 @@ CREATE TABLE sales (
 	release_date text,
 	title text,
 	genre text,
-	international_box_office integer,
+	international_box_office BIGINT,
 	domestic_box_office text,
-	worldwide_box_office integer
+	worldwide_box_office BIGINT
+
 );
