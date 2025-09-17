7fbef48e6982841da5c34c5cf6ea615f16f9f5e7
StefanSpruijt <steef.spruijt@gmail.com>
2025-09-17 16:31:22 +0200

    Add files via upload

---
+++ CREATE TABLE sales.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales (
+	sales_id integer,
+	relyear integer,
+	release_date text,
+	title text,
+	genre text,
+	international_box_office integer,
+	domestic_box_office text,
+	worldwide_box_office integer
+);
