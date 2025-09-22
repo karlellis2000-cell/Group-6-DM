9359caddff86ffc24afdc8b92cb422ad440967e1
StefanSpruijt <steef.spruijt@gmail.com>
2025-09-22 15:30:08 +0200

    Add files via upload

--- CreatingTitleReldateSales.sql
+++ CreatingTitleReldateSales.sql
@@ -1,6 +1,11 @@
 DELETE FROM sales
 WHERE release_date !~ '[0-9]';
 
+DELETE FROM sales
+WHERE release_date ~ '\(.*\)';
+
+--Deletes all lines where there is no date. E.g. 'Spring' 'Summer'
+
 UPDATE sales
 SET release_date =
   to_char(
@@ -8,7 +13,7 @@ SET release_date =
       -- e.g. "1st of january" or "15th january"
       WHEN release_date ~ '^[0-9]'
         THEN to_date(
-               regexp_replace(lower(release_date), '\s+of\s+', ' '),  -- "1st of january" → "1st january"
+               regexp_replace(lower(release_date), '\s+of\s+', ' '),  -- "1st of january" → "1st january" removes 'of'
                'DDth Month'
              )
       -- e.g. "January 15th"
@@ -23,11 +28,15 @@ SET release_date =
 ALTER TABLE sales
 ADD COLUMN full_rel_date text;
 
+--Year and date are seperate. Creating new column with full date
+
 UPDATE sales
 SET full_rel_date = to_char(
     to_date(release_date || '-' || relyear::text, 'DD-MM-YYYY'),
     'YYYY-MM-DD'
 );
+
+-- Adding year to date and making it in the form YYYY-MM-DD because it's also like this in Metaclean
 
 ALTER TABLE sales
 ADD COLUMN title_year TEXT;
@@ -36,3 +45,5 @@ UPDATE sales
 SET title_year =
     lower(regexp_replace(title, '[^a-zA-Z0-9]', '', 'g'))
     || full_rel_date::text;
+
+--Making Title_Year to make a key to link with movies table
